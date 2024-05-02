222bf160dc4d3a583d1c2355b3393c35545d6644
JMARyA <jmarya@hydrar.de>
2024-05-02 13:35:13 +0200

    fix user already exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-echo "Creating User ${UID:-1000}"
-useradd -m -u "${UID:-1000}" hoard || exit 1
+# Check if the user already exists
+if id hoard &>/dev/null; then
+    echo "User hoard already exists."
+else
+    # Create the user
+    echo "Creating User ${UID:-1000}"
+    useradd -m -u "${UID:-1000}" hoard || exit 1
+fi
 
+# Perform other setup tasks
 chown -R hoard /download
-mkdir /.cache && chown -R hoard /.cache
+mkdir -p /.cache && chown -R hoard /.cache
 chown -R hoard /data
 
+# Start the application as the user
 su hoard -c /hoard
